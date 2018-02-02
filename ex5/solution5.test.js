ed57f060356d530dfb085fcd1fb399daf387f6cc
parth <srivastavaparth2016@gmail.com>
2018-02-02 08:32:04 +0530

    test 1 for solution 5 for checking status code

---
+++ ex5/solution5.test.js
@@ -0,0 +1,39 @@
+// const http = require('http');
+const server = require('./solution5');
+
+// const promise = new Promise((resolve) => {
+//   http.get('http://localhost:8080/foo/bar/baz/index.html', (res) => {
+//     res.on('data', (chunk) => {
+//       data = chunk.toString();
+//       resolve(data);
+//     });
+//   });
+// });
+
+const option = {
+  method: 'GET',
+  url: '/?name=handling',
+};
+const f =
+`<html>
+    <head><title>Hello handling</title></head>
+    <body>
+        Hello handling
+    </body>
+</html>`;
+describe('checking the use case by using http.get', () => {
+  // it('checking if the value that is recieved is right', () => promise.then(value => expect(value).toBe(f)));
+  it('checking using injection', (done) => {
+    server.inject(option, (response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+//   it('checking using injection result', (done) => {
+//     server.inject(option, (response) => {
+//       expect(response.result).toBe(f);
+//       done();
+//     });
+//   });
+});
+
